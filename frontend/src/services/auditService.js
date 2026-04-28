a086e1368bea67844375b3c54da7d0712b4e873c
Matouš Chmelíček <chmatous@gmail.com>
2026-04-28 22:59:48 +0200

    ep for audit

--- frontend/src/services/auditService.js
+++ frontend/src/services/auditService.js
@@ -2,7 +2,7 @@ import api from '@/plugins/axios'
 
 export const auditService = {
   async getAuditLogs(filters = {}) {
-    const response = await api.get('/audit', { params: filters })
+    const response = await api.get('/audit/', { params: filters })
     return response.data
   },
 }
